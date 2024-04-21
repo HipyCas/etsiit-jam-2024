2b0860c73e66b0c74adbb654dca04e2af32a89dc
Hipy Cas <hipycas@gmail.com>
2024-04-21 16:23:04 +0200

    planets & drone

---
+++ globals/DroneManager.gd
@@ -0,0 +1,62 @@
+extends Node
+
+var _ongoing_mission = null
+var _ongoing_started_on = 0
+
+var daily_missions = []
+
+func _ready():
+	GameTime.add_daily_hook(switch_day)
+	gen_daily_missions()
+
+func _process(delta):
+	if (mission_ongoing() and (
+		(
+			(Time.get_ticks_msec() / 1000) - _ongoing_started_on)
+			>= _ongoing_mission.mission_length_real_seconds)
+		):
+		complete_mission()
+
+func gen_daily_missions():
+	daily_missions = gen_n_missions(Planet.get_current_planet(), 8)
+
+func switch_day():
+	cancel_mission()
+	gen_daily_missions()
+
+func cancel_mission():
+	_ongoing_mission = null
+
+func complete_mission():
+	pass
+
+func mission_ongoing():
+	return not _ongoing_mission == null
+
+func lauch_mission(mission_idx: int):
+	if _ongoing_mission != null:
+		return false
+	_ongoing_mission = daily_missions[mission_idx]
+	_ongoing_started_on = Time.get_ticks_msec() / 1000
+	return true
+
+func gen_mission(planet: int):
+	var conds = Loot.get_conditions_object(planet)
+	
+	var mission_length_ingame_hours = Loot.random.randi_range(2, 6)
+	var mission_length_real_seconds = (mission_length_ingame_hours * 60 * 60) / GameTime.MULTIPLIER
+	
+	return {
+		'conds': conds,
+		'mission_length_real_seconds': mission_length_real_seconds,
+		'mission_length_ingame_hours': mission_length_ingame_hours,
+		'planet': planet,
+		'coords': [Loot.random.randi_range(0, 99), Loot.random.randi_range(0, 99)]
+	}
+
+
+func gen_n_missions(planet: int, n: int):
+	var miss = []
+	for _i in range(n):
+		miss.push_back(gen_mission(planet))
+	return miss
---
+++ globals/Planet.gd
@@ -0,0 +1,64 @@
+extends Node
+
+const PLANETS = {
+	1: {
+		'name': 'Kanter',
+		'description': 'Templado para su cercanía a la estrella. Pobre en recursos. Contiene: H C Fe',
+		'motor_lvl': 0,
+		'cost': {}
+	},
+	2: {
+		'name': 'Sulvina',
+		'description': 'Cálido. Variedad de zonas áridas y húmedas. Contiene: H C Fe Si Cu Li',
+		'motor_lvl': 1,
+		'cost': {
+			'hydrogen': 50,
+			'electricity': 50,
+		}
+	},
+	3: {
+		'name': 'Briker',
+		'description': 'Muy frío. Caída regular de cometas. Contiene: H C Fe Si Cu Li W Pd',
+		'motor_lvl': 2,
+		'cost': {
+			'hydrogen': 100,
+			'electricity': 150,
+		}
+	},
+	4: {
+		'name': 'Outer space (Espacio exterior)',
+		'description': 'Continuar el viaje programado',
+		'motor_lvl': 3,
+		'cost': {
+			'hydrogen': 170,
+			'electricity': 250,
+		}
+	}
+}
+
+var _current_planet = 1
+
+
+func can_travel_to(planet: int):
+	if planet < 1 or planet > PLANETS.size():
+		return false
+	
+	var costs_match = true
+	for element in PLANETS[planet].cost.keys():
+		if not Inventory.inventory.has(element):
+			return false
+		elif Inventory.inventory[element] < PLANETS[planet].cost[element]:
+			costs_match = false
+	
+	return costs_match and Inventory.inventory.Motor >= PLANETS[planet].motor_lvl
+
+func goto_planet(planet: int):
+	if can_travel_to(planet):
+		DroneManager.cancel_mission()
+		_current_planet = planet
+
+func get_current_planet():
+	return _current_planet
+
+func get_planet(planet: int):
+	return null if planet < 1 or planet > PLANETS.size() else PLANETS[planet]
