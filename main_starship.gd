044fe2db529e0837002d79600bbcb71dea3b92aa
Hipy Cas <hipycas@gmail.com>
2024-04-21 18:28:37 +0200

    join scenes and overlays

--- main_starship.gd
+++ main_starship.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 const MAP_SCENE = preload("res://PlanetSelection.tscn")
+const CALDERA_OVERLAY = preload("res://caldera_provisional.tscn")
+const DRONE_OVERLAY = preload("res://DroneMissionSelect.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,4 +19,20 @@ func _on_static_mapa_input_event(camera, event, position, normal, shape_idx):
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			var sc = MAP_SCENE.instantiate()
 			get_node("/root/Main").visible = false
+			get_tree().root.add_child(sc)
+
+
+func _on_static_caldera_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var sc = CALDERA_OVERLAY.instantiate()
+			#get_node("/root/Main").visible = false
+			get_tree().root.add_child(sc)
+
+
+func _on_static_dron_control_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var sc = DRONE_OVERLAY.instantiate()
+			#get_node("/root/Main").visible = false
 			get_tree().root.add_child(sc)
